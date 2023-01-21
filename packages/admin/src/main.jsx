023d04c7ee1095a3a0b83d24c85a92692096d2dc
zjy4fun <zjy4fun@gmail.com>
2023-01-22 01:04:46 +0800

    🔧 build: add react-router

--- packages/admin/src/main.jsx
+++ packages/admin/src/main.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './error-page';
+import Root from './routes/root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
